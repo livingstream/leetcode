2958a1bb2afd87e742e1cc5ddd7840073ebc8c84
Anson Qian <anson627@gmail.com>
2012-11-03 08:50:32 -0700

    refactoring

--- ZigZagConversion/ZigZagConversion.cpp
+++ ZigZagConversion/ZigZagConversion.cpp
@@ -17,41 +17,40 @@
 #include <iostream>
 using namespace std;
 
-class Solution 
+class Solution
 {
 public:
     string convert(string s, int nRows)
-	{
-		if (nRows < 2) return s;
-		int N = s.size();
-		int L = 2 * (nRows - 1); // provide offset
-		string res;
-		res.clear();
-		for (int i = 0; i < N; i += L) 
-		{
-			res.push_back(s[i]);
-		}
-		for (int i = 1; i < nRows - 1; i++) 
-		{
-			for (int j = i; j < N; j += L) 
-			{
-				res.push_back(s[j]);
-				// j - i: provide offset L for each row
-				// L - i: provide reverse for each column
-				int k = (j - i) + (L - i);
-				if (k < N) res.push_back(s[k]);
-			}
-		}
-		for (int i = nRows - 1; i < N; i += L) 
-		{
-			res.push_back(s[i]);
-		}
-		return res;
-	}
+    {
+        if (nRows < 2) return s;
+        int N = s.size();
+        int L = 2 * (nRows - 1); // provide offset
+        string res;
+        res.clear();
+        for (int i = 0; i < N; i += L)
+        {
+            res.push_back(s[i]);
+        }
+        for (int i = 1; i < nRows - 1; i++)
+        {
+            for (int j = i; j < N; j += L)
+            {
+                res.push_back(s[j]);
+                // j - i: provide offset L for each row
+                // L - i: provide reverse for each column
+                int k = (j - i) + (L - i);
+                if (k < N) res.push_back(s[k]);
+            }
+        }
+        for (int i = nRows - 1; i < N; i += L)
+        {
+            res.push_back(s[i]);
+        }
+        return res;
+    }
 };
 
-int main() 
+int main()
 {
-	cout << "hello" << endl;
-	return 0;
+    return 0;
 }
